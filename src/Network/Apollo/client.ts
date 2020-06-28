1fdf5812aa8894e82547b053bdbc7bf10cad82b8
cb-adarsh <adarsh@chargebee.com>
2020-06-28 20:43:32 +0530

    changes gql endpoint

--- src/Network/Apollo/client.ts
+++ src/Network/Apollo/client.ts
@@ -1,7 +1,7 @@
 import ApolloClient from 'apollo-boost';
 
 const client = new ApolloClient({
-    uri: 'http://catalogapplication-env-1.eba-sx47xqds.ap-south-1.elasticbeanstalk.com/graphql',
+    uri: 'http://lancincatalog.me/graphql',
     // uri: 'http://192.168.1.100:9002/graphql',
 });
 
