87535c3553adffcf4a3aad36248b5287f114da35
cb-adarsh <adarsh@chargebee.com>
2020-06-29 16:49:46 +0530

    change graphql eendpoint

--- src/Network/Apollo/client.ts
+++ src/Network/Apollo/client.ts
@@ -1,7 +1,7 @@
 import ApolloClient from 'apollo-boost';
 
 const client = new ApolloClient({
-    uri: 'http://lancincatalog.me/graphql',
+    uri: '/graphql',
     // uri: 'http://192.168.1.100:9002/graphql',
 });
 
