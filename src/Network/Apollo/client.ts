b86dffcb5e8910cacf067ed796173fe789253042
cb-adarsh <adarsh@chargebee.com>
2020-06-29 16:56:16 +0530

    'change endpoints wildcard'

--- src/Network/Apollo/client.ts
+++ src/Network/Apollo/client.ts
@@ -1,8 +1,8 @@
 import ApolloClient from 'apollo-boost';
 
 const client = new ApolloClient({
+    // uri: 'http://lancincatalog.me/graphql',
     uri: '/graphql',
-    // uri: 'http://192.168.1.100:9002/graphql',
 });
 
 export default client;
