5d193709b8f5fbf06c493bebc428fa70e487609f
cb-adarsh <adarsh@chargebee.com>
2020-07-13 22:58:35 +0530

    'all cards'

--- src/comps/CardsAcepted.tsx
+++ src/comps/CardsAcepted.tsx
@@ -3,7 +3,7 @@ import '../css/cardAccepted.scss'
 
 export default function CardsAccepted() {
     return <div className='cards-accepted'>
-        <div className='cards-accepted__text'>We accept all cards</div>
+        <div className='cards-accepted__text'>All cards accepted</div>
         <div className='cards-accepted__logo-container'>
             <img className='cards-accepted__logo' src="/assets/images/american-express.png" alt="american card"/>
             <img className='cards-accepted__logo' src="/assets/images/mastercard.png" alt="mastercard card"/>
