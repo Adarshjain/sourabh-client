75719b6867be563bd98be31b307d7ca50eac549b
cb-adarsh <adarsh@chargebee.com>
2020-07-17 20:25:00 +0530

    'graphql endpoint update'

--- src/Network/Apollo/client.ts
+++ src/Network/Apollo/client.ts
@@ -1,7 +1,7 @@
 import ApolloClient from 'apollo-boost';
 
 const client = new ApolloClient({
-    // uri: 'http://lancincatalog.me/graphql',
+    // uri: 'http://suniljewellerscatalog-env.eba-qzd8ixja.ap-south-1.elasticbeanstalk.com/graphql',
     uri: '/graphql',
 });
 
