bbb85a7c51ae3335f0aa4ea14acf3070d71bca42
cb-adarsh <adarsh@chargebee.com>
2020-08-30 21:40:30 +0530

    'update endpoint'

--- src/Network/Apollo/client.ts
+++ src/Network/Apollo/client.ts
@@ -1,8 +1,8 @@
 import ApolloClient from 'apollo-boost';
 
 const client = new ApolloClient({
-    uri: 'http://suniljewellerscatalog-env.eba-qzd8ixja.ap-south-1.elasticbeanstalk.com/graphql',
-    // uri: '/graphql',
+    // uri: 'http://suniljewellerscatalog-env.eba-qzd8ixja.ap-south-1.elasticbeanstalk.com/graphql',
+    uri: '/graphql',
 });
 
 export default client;
